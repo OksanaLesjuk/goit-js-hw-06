09ff886f3f077df5561fbbff87ba4ee2ffa60e76
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-06-19 22:25:46 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,31 @@
-console.dir(document.querySelector("ul"));
+// Напиши скрипт, який:
 
-console.log(window);
-console.log("ghbdsn");
+// Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+// Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+// Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
+
+// В результаті, в консолі будуть виведені наступні повідомлення.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
+
+
+
+const listItem = document.querySelector("#categories").querySelectorAll(".item");
+
+console.log(`Number of categories: ${listItem.length}`);
+
+listItem.forEach(item =>
+    console.log(`Category: ${item.firstElementChild.textContent}
+Elements: ${item.lastElementChild.children.length}`)
+
+)
