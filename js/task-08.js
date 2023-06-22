19eb52138db502b8f8ced6e0fd5bccca3eb7cfde
OksanaLesjuk <126483028+OksanaLesjuk@users.noreply.github.com>
2023-06-22 18:10:48 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -4,3 +4,23 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+
+const userForm = document.querySelector('.login-form')
+
+console.dir(userForm);
+
+userForm.addEventListener('submit', onUserSubmit)
+
+function onUserSubmit (evt){
+    evt.preventDefault();
+    const {
+        elements: { email, password }
+      } = evt.currentTarget;
+
+      if (email.value === "" || password.value === ""){
+        alert('Всі поля повинні бути заповнені!');
+      } else {
+      console.log(`Email: ${email.value}, Password: ${password.value}`)
+      evt.currentTarget.reset()}
+}
